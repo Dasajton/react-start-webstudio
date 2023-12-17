1687dfab54f6219f50b11524b1c1b70eac72fcb3
Dasajton <david.sajitz.94@gmail.com>
2023-12-17 04:48:32 +0100

    hardcode hover text-color

--- src/components/DropdownLink.jsx
+++ src/components/DropdownLink.jsx
@@ -5,7 +5,7 @@ const DropdownLink = ({ to, handleClick, children }) => {
     <Link
       onClick={handleClick}
       to={to}
-      className="dropdown-link py-4 w-3/4 rounded-lg text-white transition ease-in-out hover:text-lightgreen hover:bg-white"
+      className="dropdown-link py-4 w-3/4 rounded-lg text-white text-center transition ease-in-out hover:text-[#74C69D] hover:bg-white"
     >
       {children}
     </Link>
