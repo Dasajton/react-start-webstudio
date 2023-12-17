256f62b5535e1a4bcf1a90a85832000030e540a6
Dasajton <david.sajitz.94@gmail.com>
2023-12-17 04:18:27 +0100

    implement burgermenu button (without function yet)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,19 +9,22 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="w-full bg-lightgreen text-white font-work flex justify-between items-center px-32 py-8">
+    <nav className="w-full bg-lightgreen text-white font-work flex justify-between items-center px-8 lg:px-32 py-8">
       <div className="logo-wrapper flex items-center gap-4">
         <img className="w-16" src="./images/logo.svg" alt="logo" />
         <h2 className="text-4xl font-medium tracking-wider">Start</h2>
       </div>
 
-      <ul className="text-lg space-x-8">
+      <ul className="text-lg space-x-8 hidden lg:block">
         {links.map((link, index) => (
           <MenuLink key={index} to={link.path}>
             {link.label}
           </MenuLink>
         ))}
       </ul>
+      <button className="lg:hidden">
+        <img src="./images/burgermenu.svg" alt="burger menu" />
+      </button>
     </nav>
   );
 };
