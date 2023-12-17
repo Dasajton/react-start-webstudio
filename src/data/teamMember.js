9756708bf638b8e1a78cded529714f1b32184abe
Dasajton <david.sajitz.94@gmail.com>
2023-12-17 23:41:16 +0100

    team-members data

---
+++ src/data/teamMember.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    firstName: 'Peg',
+    lastName: 'Legge',
+    title: 'CEO',
+    img: './images/peg.svg'
+  },
+  {
+    id: 2,
+    firstName: 'Richard',
+    lastName: 'Guerra',
+    title: 'CTO',
+    img: './images/richard.svg'
+  },
+  {
+    id: 3,
+    firstName: 'Alexandra',
+    lastName: 'Scholz',
+    title: 'Designer',
+    img: './images/alexandra.svg'
+  },
+  {
+    id: 4,
+    firstName: 'Janet',
+    lastName: 'Bray',
+    title: 'Developer',
+    img: './images/janet.svg'
+  }
+];
